1d2b3ec95fe01531f6d11f99888cf0d4e6a9f510
Büşra Ferşadoğlu <99492479+BusraFersadoglu@users.noreply.github.com>
2023-02-09 12:00:25 +0300

    Fix img src link

--- components/project.js
+++ components/project.js
@@ -1,13 +1,20 @@
-import Image from "next/image";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import p1 from "../public/image/p1.png";
+import p2 from "../public/image/p2.png";
+import p3 from "../public/image/p3.png";
+import p4 from "../public/image/p4.png";
+import p5 from "../public/image/p5.png";
+import p6 from "../public/image/p6.png";
+
 export default function Project() {
+  console.log(p1.src, "p1");
   return (
     <div id="project">
       <h1 className="h1-title">Projects</h1>
       <div className="projects">
         <div className="project">
           <h1>Movie App</h1>
-          <Image src="/../public/image/p1.png" width={400} height={200} />
+          <img src={p1.src} />
           <div className="repo">
             <a
               href="https://github.com/BusraFersadoglu/First-Next.js-Project"
@@ -19,7 +26,7 @@ export default function Project() {
         </div>
         <div className="project">
           <h1>Star Wars StarShips</h1>
-          <Image src="/../public/image/p5.png" width={400} height={200} />
+          <img src={p5.src} />
           <div className="repo">
             <a
               href="https://github.com/BusraFersadoglu/StarWars-App"
@@ -31,7 +38,7 @@ export default function Project() {
         </div>
         <div className="project">
           <h1>JS30 Challange</h1>
-          <Image src="/../public/image/p6.png" width={400} height={200} />
+          <img src={p5.src} />
           <div className="repo">
             <a
               href="https://github.com/BusraFersadoglu/JS30-Series"
@@ -43,7 +50,7 @@ export default function Project() {
         </div>
         <div className="project">
           <h1>Password Generator</h1>
-          <Image src="/../public/image/p2.png" width={400} height={200} />
+          <img src={p2.src} />
           <div className="repo">
             <a
               href="https://github.com/BusraFersadoglu/Password-Generator"
@@ -55,7 +62,7 @@ export default function Project() {
         </div>
         <div className="project">
           <h1>Random Choice Picker</h1>
-          <Image src="/../public/image/p3.png" width={400} height={200} />
+          <img src={p3.src} />
           <div className="repo">
             <a
               href="https://github.com/BusraFersadoglu/Random-Choice-Picker"
@@ -67,7 +74,7 @@ export default function Project() {
         </div>
         <div className="project">
           <h1>XOX Game</h1>
-          <Image src="/../public/image/p4.png" width={400} height={200} />
+          <img src={p4.src} />
           <div className="repo">
             <a
               href="https://github.com/BusraFersadoglu/XOX-Game"
